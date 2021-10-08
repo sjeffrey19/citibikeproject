9ac3292dccbfa961e322ad8cdf5fdc7345bd4332
Shaun Gutierrez <shaungutierrez@Shauns-iMac.lan>
2021-10-07 20:09:31 -0700

    add completed logic.js file

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,43 +2,57 @@
 d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(createMarkers);
 
 function createMarkers(response) {
-  console.log(response);
 
   // Create variable stations and assign to it response.data.stations
-  
+  var stations = response.data.stations; 
 
   // Initialize an array to hold bike markers.
-  
+  var bikeMarkers = [];
 
   // Loop through the stations array.
-  // for (   ) {
+  for (var i = 0; i < stations.length; i++) {
+    var station = stations[i];
     
-
     // For each station, create a marker, and bind a popup with the station's name.
-    
+    var bikeMarker = L.marker([station.lat, station.lon])
+    .bindPopup("<b>Station Name:</b> " + station.name 
+    + "<br><b>Payment Methods:</b> " + station.rental_methods
+    + "<br><b>Capacity:</b> " + station.capacity);
 
     // Add the marker to the bikeMarkers array.
-    
-  // }
+    bikeMarkers.push(bikeMarker);
+}
 
   // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-  // createMap(   );
+  createMap(L.layerGroup(bikeMarkers));
 }
 
 function createMap(bikeStations) {
 
   // Create the tile layer that will be the background of our map.
-  
+  var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
   // Create a baseMaps object to hold the streetmap layer.
-  
+  var baseMaps = {
+    "Street Map": streetmap
+  };
 
   // Create an overlayMaps object to hold the bikeStations layer.
-  
+  var overlayMaps = {
+    "Bike Stations": bikeStations
+  };
 
   // Create the map object with options.
-  
+  var map = L.map("map-id", {
+    center: [40.73, -74.0059],
+    zoom: 12,
+    layers: [streetmap, bikeStations]
+  });
 
   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-  
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
 }
